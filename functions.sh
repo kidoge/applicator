7f624de649d84c7823bdcc52b98ebf5887283906
Kidon Son <kidonson@gmail.com>
2019-09-28 09:48:57 -0400

    Implemented function to prompt user for action

--- functions.sh
+++ functions.sh
@@ -39,66 +39,40 @@ make_backup() {
   fi
 }
 
-create_new_file() {
+PROMPT_RESPOSE_OVERWRITE=1
+PROMPT_RESPONSE_MERGE=2
+PROMPT_RESPONSE_SKIP=3
+PROMPT_OPTION_TEXT="(O)verwrite, (M)erge, or (S)kip: "
+
+promptOverwriteOrMerge() {
   local file="$1"
-  local destination="$2"
-  local line_prefix="$3"
 
-  # If file does not exist, create a new one
-  echo ${line_prefix}${CONFIG_INSTALL_START} >> ${destination}
-  cat ${file} >> ${destination}
-  echo ${line_prefix}${CONFIG_INSTALL_END} >> ${destination}
-  echo_if_verbose "Existing file not found. File created at: ${destination}"
-}
-
-confirm_diff() {
-  local file="$1"
-  local backup="$2"
-  if [ $FORCE -eq 0 ]; then
-    vimdiff ${destination} ${backup}
-    if [ ! $? -eq 0 ]; then
-      echo_if_verbose "Rolling back changes for: ${destination}"
-      cp ${backup} ${destination}
-    fi
-  fi
-}
-
-update_existing_file() {
-  local file="$1"
-  local destination="$2"
-  local line_prefix="$3"
-
-  local backup=$(make_backup $file)
-
-  sed "/${CONFIG_INSTALL_START}/,$ d" ${backup} > ${destination}
-  echo ${line_prefix}${CONFIG_INSTALL_START} >> ${destination}
-  cat ${file} >> ${destination}
-  echo ${line_prefix}${CONFIG_INSTALL_END} >> ${destination}
-  sed "1,/${CONFIG_INSTALL_END}/ d" $backup >> ${destination}
-  echo_if_verbose "File updated at: ${destination}"
-
-  if diff ${destination} ${backup}; then
-    echo "No change detected for file: ${destination}"
-  else
-    confirm_diff ${destination} ${backup}
-  fi
+  printf "A file already exists at: $1.\n${PROMPT_OPTION_TEXT}"
+  while : ; do
+    read char
+    case "$char" in
+      "o" | "O")
+        return $PROMPT_RESPONSE_OVERWRITE
+        ;;
+      "m" | "M")
+        return $PROMPT_RESPONSE_MERGE
+        ;;
+      "s" | "S")
+        return $PROMPT_RESPONSE_SKIP
+        ;;
+    esac
+    printf "Invalid input.\n${PROMPT_OPTION_TEXT}"
+  done
 }
 
 install_config_file() {
   local file="$1"
   local destination="${INSTALL_DIR}/$(basename ${file})"
 
-  # Sanity check on input
-  if detect_already_applied $file; then
-    die "This file has already been applicated"
-  fi
-
-  local line_prefix=$(get_comment_prefix $file)
-
   if [ ! -f $destination ]; then
-    create_new_file $file $destination $line_prefix
+    cp ${file} ${destination}
   else
-    update_existing_file $file $destination $line_prefix
+    result=promptOverwriteOrMerge $destination
   fi
 }
 
